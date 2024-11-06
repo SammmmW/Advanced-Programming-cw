62a84e44c0c6210b21e38be39de1d68b30863bef
SammmmW <158096243+SammmmW@users.noreply.github.com>
2024-11-06 20:16:59 +0000

    Add files via upload
    Early GUI files - contains the essential features (sort of) but has no connection to F# files

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Click(object sender, RoutedEventArgs e)
+        {
+            Result.Text = "Hello!";
+        }
+        private void Help_Click(object sender, RoutedEventArgs e)
+        {
+            //Error.Text = "I am an error";
+            Info.Text = "Accepted tokens are as follows:\nAll digits (1-9), +, -, * (multiply), /(divide), % (remainder), ^(to the power of), (), . (for decimal places), E (exponent), Sin, Cos, Tan, Pi, Log";
+        }
+    }
+}
