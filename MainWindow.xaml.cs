a379701c9ffdcbcce4e80c47f2c1b5179611a5c8
SammmmW <158096243+SammmmW@users.noreply.github.com>
2024-11-10 20:05:39 +0000

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,9 +2,6 @@ using System.Windows;
 
 namespace WpfApp1
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         bool helpActive = false;
@@ -15,18 +12,20 @@ namespace WpfApp1
 
         private void Btn_Click(object sender, RoutedEventArgs e)
         {
-            Result.Text = "Hello!";
+            Result.Text = Input.Text; //in the final program, what this needs to actually do is pass 'Input.Text' into the f# program, then display that result
         }
         private void Help_Click(object sender, RoutedEventArgs e)
         {
             helpActive = !helpActive;
             if (helpActive)
             {
-                Info.Text = "Accepted tokens are as follows:\nAll digits (1-9), +, -, * (multiply), /(divide), % (remainder), ^(to the power of), (), . (for decimal places), E (exponent), Sin, Cos, Tan, Pi, Log";
+                Info.Text = "Accepted tokens are as follows:\nAll digits (0-9), +, -, * (multiply), / (divide), % (remainder), ^ (to the power of), (), . (for decimal places), E (exponent), sqrt (Square Root), Sin, Cos, Tan, Pi, Log";
+                Help.Content = "Close";
             }
             else
             {
                 Info.Text = "";
+                Help.Content = "Info";
             }
         }
     }
