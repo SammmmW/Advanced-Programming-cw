1b9c57e002a93729e66ecdcc257a32c8c40a4a6a
SammmmW <158096243+SammmmW@users.noreply.github.com>
2024-11-11 13:53:42 +0000

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        bool helpActive = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Click(object sender, RoutedEventArgs e)
+        {
+            Result.Text = Input.Text; //in the final program, what this needs to actually do is pass 'Input.Text' into the f# program, then display that result
+        }
+        private void Help_Click(object sender, RoutedEventArgs e)
+        {
+            helpActive = !helpActive;
+            if (helpActive)
+            {
+                Info.Text = "Accepted tokens are as follows:\nAll digits (0-9), +, -, * (multiply), / (divide), % (remainder), ^ (to the power of), (), . (for decimal places), E (exponent), sqrt (Square Root), Sin, Cos, Tan, Pi, Log";
+                Help.Content = "Close";
+            }
+            else
+            {
+                Info.Text = "";
+                Help.Content = "Info";
+            }
+        }
+    }
+}
