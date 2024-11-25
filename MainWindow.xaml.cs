d29611ba8dcf55f1f66a8b487a8c2e8c721a180d
SammmmW <158096243+SammmmW@users.noreply.github.com>
2024-11-25 13:52:54 +0000

    Update MainWindow.xaml.cs
    Changed info text box with new command words

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,3 +1,5 @@
+using ClassLibrary1;
+using Microsoft.FSharp.Collections;
 using System.Windows;
 
 namespace WpfApp1
@@ -12,14 +14,18 @@ namespace WpfApp1
 
         private void Btn_Click(object sender, RoutedEventArgs e)
         {
-            Result.Text = Input.Text; //in the final program, what this needs to actually do is pass 'Input.Text' into the f# program, then display that result
+            //Result.Text = Input.Text; //in the final program, what this needs to actually do is pass 'Input.Text' into the f# program, then display that result
+            var fsString = lexparser.str2lst(Input.Text);
+            var oList = lexparser.lexer(fsString);
+            double b = lexparser.parseNeval(oList).Item2;
+            Result.Text = b.ToString();
         }
         private void Help_Click(object sender, RoutedEventArgs e)
         {
             helpActive = !helpActive;
             if (helpActive)
             {
-                Info.Text = "Accepted tokens are as follows:\nAll digits (0-9), +, -, * (multiply), / (divide), % (remainder), ^ (to the power of), (), . (for decimal places), E (exponent), sqrt (Square Root), Sin, Cos, Tan, Pi, Log";
+                Info.Text = "Accepted tokens are as follows:\nAll digits (0-9), +, -, * (multiply), / (divide), % (remainder), ^ (to the power of), (), . (for decimal places), sqrt (Square Root), exp (Exponent) Sin, Cos, Tan, Pi, Log, = (for variable assignment), int, float, and ; (must append on any variable assignment)";
                 Help.Content = "Close";
             }
             else
